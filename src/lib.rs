b668d58a16ee9082bec60a89e2b13159f21f3505
kökbirasi <kokbirasi@192.168.1.37>
2024-10-15 19:37:25 +0300

    complete of Phases

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,75 @@
 /*
     1. Verilen testi miktar ve para birimine ayır.
     2. "42 Lira" -> 42 ve "Lira"
-    3. "42 Lira" -> (f64, String)
+    3. "42 Lira" -> Money
 */
 
 
-use std::{error::Error, fmt::Display, num::ParseFloatError}; 
+use core::str;
+use std::{error::Error, fmt::Display, num::ParseFloatError, str::FromStr}; 
 
 
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct Money {
+    quantity: f64,
+    currency_unit: CurrencyUnit,
+}
 
-
-pub fn parse_money(input: &str) -> Result<(f64, String), MoneyError> {
-    let pieces: Vec<&str>  = input.split_whitespace().collect();
-    // ["42", "lira"]
-    if 2 != pieces.len() {
-        Err(MoneyError {kind:MoneyErrorKind::ParseError,})
+impl FromStr for Money {
+    type Err = MoneyError;
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let pieces: Vec<&str>  = input.split_whitespace().collect();
+    
+        match pieces[..] {
+            [quantity, currency_unit] =>
+            {
+                Ok(Money::from_parts(quantity.parse()?, currency_unit.parse()?))
+            },
+            _ => Err(MoneyError {
+                kind:MoneyErrorKind::Formating,
+            })
+        }
     }
-    else {
-        let quantity: f64 = pieces[0].parse()?;
-        let currency_unit: String = pieces[1].into();
-        Ok((quantity, currency_unit))
+    
+}
+
+#[derive(Debug, Clone, PartialEq, Default)]
+pub enum CurrencyUnit {
+    #[default]
+    Lira,
+    Dolar,
+    Euro,
+}
+
+impl Money {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    
+    pub fn from_parts(quantity: f64, currency_unit: CurrencyUnit) -> Self {
+        Self {
+            quantity,
+            currency_unit,
+        }
     }
 }
+
+impl FromStr for CurrencyUnit {
+    type Err = MoneyError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_ref() {
+            "lira" | "tl" => Ok(CurrencyUnit::Lira),
+            "dolor" | "$" | "usd" => Ok(CurrencyUnit::Dolar),
+            "euro" | "€" | "eur" => Ok(CurrencyUnit::Euro),
+            _ => Err(MoneyError {
+                kind: MoneyErrorKind::CurrencyUnit
+            })
+        }
+    }
+}
+
+// Error
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct MoneyError {
@@ -30,7 +78,9 @@ pub struct MoneyError {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum MoneyErrorKind {
-    ParseError,
+    Formating,
+    Quantity,
+    CurrencyUnit,
 }
 
 
@@ -43,7 +93,11 @@ impl Error for MoneyError {
 impl Display for MoneyError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self.kind {
-            MoneyErrorKind::ParseError => write!(f, "Money parsing error")
+            MoneyErrorKind::Formating => {
+                write!(f, "In quantity and currency please.. Example: 42 Lira")
+            }
+            MoneyErrorKind::Quantity => write!(f, "Quantity parsing error"),
+            MoneyErrorKind::CurrencyUnit => write!(f, "Current unit parsing error"),
         }
     }
 }
@@ -51,7 +105,7 @@ impl Display for MoneyError {
 impl From<ParseFloatError> for MoneyError {
     fn from(_: ParseFloatError) -> Self {
         MoneyError {
-            kind: MoneyErrorKind::ParseError,
+            kind: MoneyErrorKind::Quantity,
         }
     }
 }
@@ -62,28 +116,42 @@ mod tests {
 
     #[test]
     fn positive_decimals() {
-        let result: Result<(f64, String), MoneyError> = parse_money("42.1 lira");
-        assert_eq!(Ok((42.1, "lira".to_string())), result);
+        let result: Result<Money, MoneyError> = "42.1 lira".parse::<Money>();
+        assert_eq!(Ok(Money::from_parts(42.1, CurrencyUnit::Lira)), result);
     }
     #[test]
-    fn negative_decimal() {
-        let result: Result<(f64, String), MoneyError> = parse_money("-42.1 lira");
-        assert_eq!(Ok((-42.1, "lira".to_string())), result);
+    fn negative_decimals() {
+        let result: Result<Money, MoneyError> = "-42.1 lira".parse::<Money>();
+        assert_eq!(Ok(Money::from_parts(-42.1, CurrencyUnit::Lira)), result);
     }
+ 
 
     #[test]
     fn positive_integer_number() {
-        let result: Result<(f64, String), MoneyError> = parse_money("42.1 lira");
-        assert_eq!(Ok((42.1, "lira".to_string())), result);
+        let result: Result<Money, MoneyError> = "42 lira".parse::<Money>();
+        assert_eq!(Ok(Money::from_parts(42.0, CurrencyUnit::Lira)), result);
     }
     #[test]
     fn negative_integer_number() {
-        let result: Result<(f64, String), MoneyError> = parse_money("-42.1 lira");
-        assert_eq!(Ok((-42.1, "lira".to_string())), result);
+        let result: Result<Money, MoneyError> = "-42 lira".parse::<Money>();
+        assert_eq!(Ok(Money::from_parts(-42.0, CurrencyUnit::Lira)), result);
     }
 
     #[test]
+    #[should_panic]
     fn single_item() {
-        parse_money("42").unwrap();
+        "42".parse::<Money>().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn wrong_quantity() {
+        "42a lira".parse::<Money>().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn multi_item() {
+        "42 lira dolar".parse::<Money>().unwrap();
     }
 }
