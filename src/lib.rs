a41618e747d4d3bfb4dc811c6bc975b36b8d438c
kökbirasi <kokbirasi@192.168.1.37>
2024-10-15 17:51:09 +0300

    Phase->2->3->4

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,55 @@
 /*
     1. Verilen testi miktar ve para birimine ayır.
     2. "42 Lira" -> 42 ve "Lira"
-    3. "42 Lira" -> (i32, String)
-*/  
+    3. "42 Lira" -> (f64, String)
+*/
 
 
-pub fn parse_money(input: &str) -> (i32, String) {
+use std::{error::Error, fmt::Display, num::ParseFloatError}; 
+
+
+
+
+pub fn parse_money(input: &str) -> Result<(f64, String), MoneyError> {
     let pieces: Vec<&str>  = input.split_whitespace().collect();
     // ["42", "lira"]
-    let quantity: i32  = pieces[0].parse().unwrap();
-
+    let quantity: f64 = pieces[0].parse()?;
     let currency_unit: String = pieces[1].into();
-    (quantity, currency_unit)
+    Ok((quantity, currency_unit))
 
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MoneyError {
+    kind: MoneyErrorKind,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum MoneyErrorKind {
+    ParseError,
+}
+
+
+impl Error for MoneyError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(self)
+    }
+}
+
+impl Display for MoneyError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.kind {
+            MoneyErrorKind::ParseError => write!(f, "Money parsing error")
+        }
+    }
+}
+
+impl From<ParseFloatError> for MoneyError {
+    fn from(_: ParseFloatError) -> Self {
+        MoneyError {
+            kind: MoneyErrorKind::ParseError,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -20,8 +57,24 @@ mod tests {
     use super::*;
 
     #[test]
+    fn positive_decimals() {
+        let result: Result<(f64, String), MoneyError> = parse_money("42.1 lira");
+        assert_eq!(Ok((42.1, "lira".to_string())), result);
+    }
+    #[test]
+    fn negative_decimal() {
+        let result: Result<(f64, String), MoneyError> = parse_money("-42.1 lira");
+        assert_eq!(Ok((-42.1, "lira".to_string())), result);
+    }
+
+    #[test]
     fn positive_integer_number() {
-        let result: (i32, String) = parse_money("42 lira");
-        assert_eq!((42, "lira".to_string()), result);
+        let result: Result<(f64, String), MoneyError> = parse_money("42.1 lira");
+        assert_eq!(Ok((42.1, "lira".to_string())), result);
+    }
+    #[test]
+    fn negative_integer_number() {
+        let result: Result<(f64, String), MoneyError> = parse_money("-42.1 lira");
+        assert_eq!(Ok((-42.1, "lira".to_string())), result);
     }
 }
